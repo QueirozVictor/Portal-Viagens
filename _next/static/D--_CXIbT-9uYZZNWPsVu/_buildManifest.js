066577fa8b980fcdb925d86d6a31c9a77335ccaa
Queiroz-Victor <queirozvictor915@gmail.com>
2026-08-02 15:00:24 +0900

    Updates

---
+++ _next/static/D--_CXIbT-9uYZZNWPsVu/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Portal-Viagens//_next/:path+",
+        "destination": "/Portal-Viagens/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- app/gridCompleto/page.tsx
+++
@@ -1,8 +0,0 @@
-import Grid from "../components/Grid";
-import { destinos } from "../lb/destinos";
-
-export default function Catalogo(){
-    return(
-        <Grid viagens={destinos}/>
-    )
-}
